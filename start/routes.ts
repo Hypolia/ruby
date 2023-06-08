f90834a68cc9ce426fbd1c3e4d926f31c303d2e7
Nathael Bonnal <kirua48000@gmail.com>
2023-06-08 12:00:04 +0200

    :construction: add auth and authorization

--- start/routes.ts
+++ start/routes.ts
@@ -24,4 +24,20 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 
+Route.group(() => {
+  Route.group(() => {
+
+  }).prefix('/authentication')
+
+  Route.group(() => {
+    Route.group(() => {
+      Route.get('/', 'AccountsController.index')
+      Route.get('/:id', 'AccountsController.show')
+      Route.post('/', 'AccountsController.store')
+      Route.put('/:id', 'AccountsController.update')
+      Route.delete('/:id', 'AccountsController.destroy')
+    }).prefix('/accounts')
+  }).middleware(['auth'])
+}).prefix('/v1')
+
 Route.get('/users', 'UsersControler.index')
